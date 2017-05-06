dd9065a81de9d1821d6ebad8b5e2468f4eea9e40
Artem Kostenko <kosten27@gmail.com>
2017-05-06 17:30:39 +0300

    Created new project

---
+++ app/src/main/java/ua/sten/workout/MainActivity.java
@@ -0,0 +1,16 @@
+package ua.sten.workout;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WorkoutDetailFragment frag = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_frag);
+        frag.setWorkoutId(1);
+    }
+}
---
+++ app/src/main/java/ua/sten/workout/WorkoutListFragment.java
@@ -0,0 +1,26 @@
+package ua.sten.workout;
+
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class WorkoutListFragment extends ListFragment {
+
+
+    public WorkoutListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
